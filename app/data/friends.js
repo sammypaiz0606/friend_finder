fb249aaef98b0395235addea86ca1f26d2869e40
sammy paiz <sammypaiz@sammypaiz-2.local>
2019-04-16 14:05:21 -0700

    changed folders & files

---
+++ app/data/friends.js
@@ -0,0 +1,110 @@
+// Array that holds data objects, a list of all friends and applicable data
+
+// Might need to change the datatype of answers to string
+module.exports = [
+	{
+		name: "Sansa Stark",
+		photo: "http://images6.fanpop.com/image/polls/1571000/1571738_1467023584408_full.jpg",
+		answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+	},
+	{
+		name: "Winnie the Pooh",
+		photo: "https://pbs.twimg.com/profile_images/691863315853762561/ClkLQse3.jpg",
+		answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Jon Snow",
+		photo: "https://pbs.twimg.com/profile_images/844785536195739651/GkGMChWu.jpg",
+		answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Daenerys Targaryen",
+		photo: "https://pbs.twimg.com/profile_images/737267065770348544/-_wliDDo.jpg",
+		answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Petyr Baelish",
+		photo: "https://d2juyu303oh9b6.cloudfront.net/image/49f3dd7699a17da38e963d5add534749.jpg?&icq=80&sig=577bf959df2f0ac1aa1c1d500ee780a1",
+		answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+	},
+	{
+		name: "Margaery Tyrell",
+		photo: "https://pbs.twimg.com/profile_images/906447699439964161/yZoCaNDC.jpg",
+		answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
+	},
+	{
+		name: "Jaqen H'ghar",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BOGI1ZWZkODQtN2IxMy00NTNjLTk1YmYtOGM4NTk1MTI3M2FkXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
+		answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+	},
+	{
+		name: "Olenna Tyrell",
+		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s7/olenna-tyrell-512x512.jpg",
+		answers: [5, 1, 2, 5, 5, 5, 1, 3, 4, 5]
+	},
+	{
+		name: "Melisandre",
+		photo: "https://assets-auto.rbl.ms/22bd2c7094b5f310574a15e21b55450781a67c19d5ed0ad23b1abdb43b3f820f",
+		answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+	},
+	{
+		name: "Elmo",
+		photo: "https://pbs.twimg.com/profile_images/454270645879070720/82lJeUWO.jpeg",
+		answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+	},
+	{
+		name: "Tyrion Lannister",
+		photo: "http://assets.viewers-guide.hbo.com/large5527f803e45b1.jpg",
+		answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
+	},
+	{
+		name: "Drogo",
+		photo: "http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/khal-drogo-512x512.jpg",
+		answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
+	},
+	{
+		name: "Jaime Lannister",
+		photo: "https://pbs.twimg.com/profile_images/626123344891375616/BYJvXTvy.jpg",
+		answers: [3, 1, 4, 3, 5, 5, 1, 4, 3, 5]
+	},
+	{
+		name: "Joffrey Baratheon",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BYTc4NzhjZmUtYzczNi00ZDQxLTlhOTgtMzEyZjUxNjZmZjhiXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
+		answers: [5, 1, 5, 1, 5, 1, 1, 2, 2, 1]
+	},
+	{
+		name: "Oberyn-Martell",
+		photo: "http://vignette1.wikia.nocookie.net/gameofthrones/images/9/94/Oberyn-Martell-family-tree.jpg/revision/latest?cb=20140410141001",
+		answers: [5, 4, 5, 3, 5, 3, 4, 4, 5, 5]
+	},
+	{
+		name: "Missandei",
+		photo: "https://pbs.twimg.com/profile_images/871944819354075138/dTFQQ36_.jpg",
+		answers: [3, 1, 3, 5, 2, 1, 1, 5, 5, 5]
+	},
+	{
+		name: "Brienne",
+		photo: "http://game-of-thrones.club/wp-content/uploads/2017/05/brienne-of-tarth-512x512-1.jpg",
+		answers: [3, 1, 1, 5, 5, 5, 1, 5, 5, 5]
+	},
+	{
+		name: "Robb Stark",
+		photo: "https://topicimages.mrowl.com/large/chaseosburn/chaos_is_a_ladder/actors/richard_madden__robb_stark_1.jpg",
+		answers: [5, 3, 4, 3, 5, 5, 1, 5, 4, 5]
+	},
+	{
+		name: "Theon Greyjoy",
+		photo: "https://pbs.twimg.com/profile_images/748890621843271680/p-_aNCMB.jpg",
+		answers: [5, 5, 5, 1, 5, 1, 1, 3, 3, 3]
+	},
+	{
+		name: "Renly Baratheon",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BM2I1NTgwZDUtYzI3ZC00MzY3LWJhNmYtYWE0NmNkYjA3YWJhXkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
+		answers: [4, 5, 4, 1, 5, 3, 2, 3, 4, 5]
+	}
+
+];
+
+// module.exports.push2arr = function(data) {
+// 	module.exports.allFriends.push(data);
+// }
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,103 @@
+// ***** DEPENDENCIES *****
+var friends = require("../data/friends.js");
+var path = require("path");
+var fs = require("fs");
+
+// ***** API ROUTES *****
+module.exports = function(app){
+
+	// ---- Get a json list of all available friends ----
+	app.get("/api/friends", function(req, res) {
+	  res.json(friends);
+	});
+
+	// ---- POST request used for survey ----
+	app.post("/api/friends", function(req, res){
+		console.log("this will do stuff");
+		var friendInput = req.body; // stores input from user survey
+		res.json(true);
+		console.log("\nName: " + friendInput.name + "\nPhoto: " +
+			friendInput.photo + "\nScores: " + friendInput.answers);
+
+		friendInput.answers = friendInput.answers.split(",");
+
+		friends.push(friendInput);
+		convertAnswers(friendInput);
+		// console.log(friendInput.answers);
+
+		compareFriends(friends, friendInput);
+
+		// console.log(friendInput);
+	});
+
+}
+
+// Constructor function for builiding friend objects using the
+// name, photo link, and an array of the answers
+function NewFriend(name, photo, answers) {
+	this.name = name;
+	this.photo = photo;
+	this.answers = answers;
+}
+
+// Function to change formate of survey answers because the come in as strings
+function convertAnswers(currentFriend) {
+	// variable to hold the current friend
+	var current = currentFriend;
+	// console.log(current);
+
+	var curAnswers = current.answers; // holds answers
+
+	// converts answers from strings to numbers
+	for (i=0; i<curAnswers.length; i++) {
+		curAnswers[i] = parseInt(curAnswers[i]);
+	}
+
+} // END convertAnswers()
+
+// Comparison function to compare the current friend's answers to those
+// of other friends in the list
+function compareFriends(allFriends, currentFriend) {
+	var curFriend = currentFriend.answers;
+	var matchFriend;
+	var matchScores = [];
+	var matchScore = 0;
+	var closestMatch;
+
+	// for each friend (excluding last added)...
+	for (i=0; i<allFriends.length-1; i++) {
+		// store all the scores in an array...
+		matchFriend = allFriends[i].answers;
+		// console.log(matchFriend);
+
+		// for each answer in an array...
+		for (j=0; j<matchFriend.length; j++) {
+			// store the abs value of the difference between the answers of
+			// the new friend and this friend in the array
+			var qScore = Math.abs(curFriend[j] - matchFriend[j]);
+			// the total match score is equal to the sum of all qScores
+			matchScore += qScore;
+		} // End of scoring for loop
+		// console.log(matchScore)
+
+		// push this friends matchScore into an array
+		matchScores.push(matchScore);
+		// reset the matchScore to zero before moving to the next friend
+		matchScore = 0;
+	} // END of main for loop
+	// console.log(matchScores)
+
+	// Find lowest score in matchScores array
+	var lowestScore = Math.min(...matchScores);
+	// console.log(lowestScore);
+
+	// find the index of the lowest score
+	var matchIndex = matchScores.indexOf(lowestScore);
+	// console.log(matchIndex);
+	// find the friend at this index in the allFriends array
+	var bestFriend = allFriends[matchIndex];
+
+	// add a new property to the current friend's object that holds the best match
+	currentFriend.bestie = bestFriend;
+	// console.log(currentFriend);
+}
